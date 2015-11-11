d3ff3d6f6799b8b58598d6d568a05d874d6a4a72
hector <hherrera1229@gmail.com>
2015-11-11 22:14:33 +0000

    First commit

---
+++ temperature-screen.py
@@ -0,0 +1,44 @@
+#!/usr/env/python
+import os
+import glob
+import time
+import serial
+from datetime import datetime
+from sys import argv
+
+
+target = open('temp.txt', 'w')
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ser = serial.Serial("/dev/serial/by-path/platform-20980000.usb-usb-0:1.2:1.0-port0",9600)
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return round(temp_f,1)
+	
+
+while True:
+	ser.write(str(read_temp()))
+	time.sleep(2)
+
+	
+
